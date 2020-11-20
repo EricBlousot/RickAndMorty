28685bab66ba1d6ec22a994daa6a03bc4b3ca2ba
Louis Corbet <ls.corbet@gmail.com>
2020-11-20 10:40:52 +0100

    Set up router and toasts

---
+++ rickandmorty_react/src/pages/notfound.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class NotFoundPage extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+          NotFoundpage
+      </div>)
+  }
+}
+
+export default NotFoundPage;
